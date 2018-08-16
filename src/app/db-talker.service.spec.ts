708da2891400756b9a3be9c5a7320257197fae28
Larry <hastings360@gmail.com>
2018-08-16 14:48:28 -0400

    asdf

--- src/app/db-talker.service.spec.ts
+++ src/app/db-talker.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick} from '@angular/core/testing';
 
 import { DbTalkerService } from './db-talker.service';
 
