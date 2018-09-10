17168ae29a7f3e1c7bc3b3c6963abcd0db56db49
Larry <hastings360@gmail.com>
2018-09-10 14:52:43 -0400

    asdf

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -54,8 +54,8 @@ export class InputFormComponent implements OnInit {
             apiObject.append('image', this.imageToApi);
 
             this.dbTalker.submitPhotoToDb(apiObject)
-              .then(() => {this.received = true; this.newPicCreated.emit(true); });
-              //.catch(error => {this.error = true; console.log(error + ': Error submitting pic to SubmitPhotoToDb()--DbTalkerService'); });
+              .then(success => {console.log('photo accepted: ' + success); this.received = true; this.newPicCreated.emit(true); })
+              .catch(error => {this.error = true; console.log(error + ': Error submitting pic to SubmitPhotoToDb()--DbTalkerService'); });
         }else {
           this.error = true;
           console.log('Token invalid or expired');
