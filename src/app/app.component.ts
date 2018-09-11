5a54746fe1e6921ad892476cece8b20fbf2315a1
Larry <hastings360@gmail.com>
2018-09-11 14:35:37 -0400

    updated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,39 +14,35 @@ import { PhotoStr } from './photo-str.model';
 export class AppComponent {
 
   public photos = new BehaviorSubject<PhotoStr[]>([]);
-  public databaseError: boolean = false;
+  public databaseError = false;
   public previewPhoto: PhotoStr = new PhotoStr;
-  public seePhoto: boolean = false;
-  public hideMain: boolean = false;
+  public seePhoto = false;
+  public hideMain = false;
 
-  constructor(private dbTalker:DbTalkerService){
+  constructor(private dbTalker: DbTalkerService) {
     this.photos = this.dbTalker.loadRecent30();
   }
 
-  newPicCreated(event:boolean):void{
-    if(event === true){
+  newPicCreated(event: boolean): void {
+    if (event === true) {
       this.photos = this.dbTalker.loadRecent30();
-    }else{
-      console.log("Error: false showing on app-root.newPicCreated when it shouldn't");
+    }else {
+      console.log('Error: false showing on app-root.newPicCreated when it should not');
     }
   }
 
-  newPhotoQuery(event:string):void{
+  newPhotoQuery(event: string): void {
     this.photos = this.dbTalker.loadSearch30(event);
   }
 
-  seePreview(x){
-    this.previewPhoto = x;
+  seePreview(selectedPhoto) {
+    this.previewPhoto = selectedPhoto;
     this.seePhoto = true;
     this.hideMain = true;
   }
 
-  closePrev(x){
-    this.seePhoto = x;
+  closePrev(selectedPhoto) {
+    this.seePhoto = selectedPhoto;
     this.hideMain = false;
   }
-
-  //testForToken(){
-  //      if(localStorage.user = this.currentUser)
-  //}
 }
